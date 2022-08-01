0126566e6bdfd3ed59707744c696d1698843be4f
TLRKiliann <philogenie@protonmail.com>
2022-08-01 21:35:21 +0200

    some modifications to test ng serve

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstDocComponent } from './first-doc/first-doc.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstDocComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
