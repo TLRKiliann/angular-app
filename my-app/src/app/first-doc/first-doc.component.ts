3116b21c94ae211486c2350e47e3f8556d8db942
TLRKiliann <philogenie@protonmail.com>
2022-08-01 21:33:47 +0200

    first-doc.component created

---
+++ my-app/src/app/first-doc/first-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-doc',
+  templateUrl: './first-doc.component.html',
+  styleUrls: ['./first-doc.component.scss']
+})
+export class FirstDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
